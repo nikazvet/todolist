2d6c220c396d9e20d6fd787e6c63432515e7a5cc
Veronika P-CB02 Student 4173287 <v.tsvietkova@student.fontys.nl>
2022-10-06 14:31:53 +0200

    Started work on a calendar

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -1,34 +1,22 @@
-<script>
-
+<script setup>
+    import { ref } from 'vue'
+    const dates = ref(["Day", "Monday 3", "Tuesday 4", "Wednesday 5", "Thursday 6", "Friday 7", "Saturday 8", "Saturday 9"])
+    const startTime = ref(9);
+    const endTime = ref(18)
+    const step = ref(0.5)
+    const noofrows = ref((endTime-startTime)/step)
+    const num = ref(noofrows);
 </script>
 
 <template>
-    <table class="border">
-        <thead>
-            <tr>
-                <th class="w-4"></th>
-                <th>Monday</th>
-                <th>Tuesday</th>
-                <th>Wednesday</th>
-                <th>Thursday</th>
-                <th>Friday</th>
-                <th>Saturday</th>
-                <th>Sunday</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <th></th>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-            </tr>
-        </tbody>  
-    </table>
+    <div class="col-span-4 grid grid-cols-8 grid-rows-[{{(endTime-startTime)*2/step}}] grid-flow-row bg-red-100">
+        <div v-for="itemdate in dates">{{itemdate}}</div>
+        <div class="bg-red-100" v-for="n in (endTime-startTime)*2/step">
+            {{startTime + n/2}}
+        </div>
+        <div class="grid-flow-row bg-yellow-100" v-for="a in (endTime-startTime)*2*7">Hour</div>
+    </div>
+    
 </template>
 
 <style>
