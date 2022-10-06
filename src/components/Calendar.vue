10c076072caf78cb762551c404804d55654b433e
Veronika P-CB02 Student 4173287 <v.tsvietkova@student.fontys.nl>
2022-10-06 15:38:59 +0200

    Calendar update

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -1,5 +1,12 @@
 <script setup>
     import { ref } from 'vue'
+import Day from './Day.vue'
+import Day1 from './Day.vue'
+import Day2 from './Day.vue'
+import Day3 from './Day.vue'
+import Day4 from './Day.vue'
+import Day5 from './Day.vue'
+import Day6 from './Day.vue'
     const dates = ref(["Day", "Monday 3", "Tuesday 4", "Wednesday 5", "Thursday 6", "Friday 7", "Saturday 8", "Saturday 9"])
     const startTime = ref(9);
     const endTime = ref(18)
@@ -9,12 +16,20 @@
 </script>
 
 <template>
-    <div class="col-span-4 grid grid-cols-8 grid-rows-[{{(endTime-startTime)*2/step}}] grid-flow-row bg-red-100">
+    <div class="col-span-4 grid grid-cols-8 auto-rows-min grid-rows-[{{(endTime-startTime)*2}}] bg-red-100">
         <div v-for="itemdate in dates">{{itemdate}}</div>
-        <div class="bg-red-100" v-for="n in (endTime-startTime)*2/step">
-            {{startTime + n/2}}
+        <div>
+            <div class="bg-red-100 col-start-1" v-for="n in (endTime-startTime)/step+1">
+                {{startTime - step + n/2}}
+            </div>
         </div>
-        <div class="grid-flow-row bg-yellow-100" v-for="a in (endTime-startTime)*2*7">Hour</div>
+        <Day></Day>
+        <Day1></Day1>
+        <Day2/>
+        <Day3></Day3>
+        <Day4></Day4>
+        <Day5></Day5>
+        <Day6></Day6>
     </div>
     
 </template>
---
+++ src/components/Day.vue
@@ -0,0 +1,28 @@
+<script setup>
+    import { ref } from 'vue'
+    const startTime = ref(9);
+    const endTime = ref(18)
+    const step = ref(0.5)
+    const noofrows = ref((endTime-startTime)/step+1)
+
+    const slots = $ref([{time: 10, name: "event"}, {time: 11, name: "event"}])
+
+    function AddSlot(time){
+        slots.push({time:time, name: "added slot"})
+    }
+</script>
+
+<template>
+    <div class="row-start">
+        <div class="bg-yellow-100 row-span-1 w-full h-6 hover:bg-blue-100" v-for="n in (endTime-startTime)/step+1">
+            <div class="bg-red-100 row-span-1 w-full h-full hover:bg-slate-100" v-if="(n/2 - step +startTime)===10 || (n/2 - step +startTime)===11">
+                Event
+            </div>
+            <button class="w-full h-full" v-else @click="AddSlot(n/2 - step +startTime)"></button>
+        </div>
+    </div>
+</template>
+
+<style>
+
+</style>
